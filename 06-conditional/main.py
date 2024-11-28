79d09705cd1ba552476903d3444a21d71fb302b9
Jeremy Hummel <jmhummel@gmail.com>
2024-11-28 04:14:28 -0500

    CIFAR development

--- 06-conditional/main.py
+++ 06-conditional/main.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import math
 import os
 import shutil
@@ -6,14 +7,93 @@ import time
 from abc import abstractmethod
 from collections import namedtuple
 from functools import partial
+from typing import Tuple, Any, Optional, Union, Callable, List
 
 import torch
 import torchvision
 import torchvision.transforms.functional as F
+from PIL import Image
 from matplotlib import pyplot as plt
 from torch.optim.swa_utils import get_ema_multi_avg_fn
 from torchvision.utils import make_grid
 from tqdm import trange
+
+CSV = namedtuple("CSV", ["header", "index", "data"])
+
+class CelebAHQ(torchvision.datasets.VisionDataset):
+    def __init__(self, root,
+                 target_type: Union[List[str], str] = "attr",
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None):
+        super().__init__(root, transform=transform, target_transform=target_transform)
+        if isinstance(target_type, list):
+            self.target_type = target_type
+        else:
+            self.target_type = [target_type]
+
+        if not self.target_type and self.target_transform is not None:
+            raise RuntimeError("target_transform is specified but target_type is empty")
+
+        attr = self._load_csv("CelebAMask-HQ-attribute-anno.txt", header=1)
+        self.filename = attr.index
+        self.attr = attr.data
+        # map from {-1, 1} to {0, 1}
+        self.attr = torch.div(self.attr + 1, 2, rounding_mode="floor")
+        self.attr_names = attr.header
+
+
+    def _load_csv(
+            self,
+            filename: str,
+            header: Optional[int] = None,
+    ) -> CSV:
+        with open(os.path.join(self.root, filename)) as csv_file:
+            data = list(csv.reader(csv_file, delimiter=" ", skipinitialspace=True))
+
+        if header is not None:
+            headers = data[header]
+            data = data[header + 1 :]
+        else:
+            headers = []
+
+        indices = [row[0] for row in data]
+        data = [row[1:] for row in data]
+        data_int = [list(map(int, i)) for i in data]
+
+        return CSV(headers, indices, torch.tensor(data_int))
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        X = Image.open(os.path.join(self.root, "CelebA-HQ-img-orig", self.filename[index]))
+
+        target: Any = []
+        for t in self.target_type:
+            if t == "attr":
+                target.append(self.attr[index, :])
+            # elif t == "identity":
+            #     target.append(self.identity[index, 0])
+            # elif t == "bbox":
+            #     target.append(self.bbox[index, :])
+            # elif t == "landmarks":
+            #     target.append(self.landmarks_align[index, :])
+            else:
+                # TODO: refactor with utils.verify_str_arg
+                raise ValueError(f'Target type "{t}" is not recognized.')
+
+        if self.transform is not None:
+            X = self.transform(X)
+
+        if target:
+            target = tuple(target) if len(target) > 1 else target[0]
+
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+        else:
+            target = None
+
+        return X, target
+
+    def __len__(self) -> int:
+        return len(self.attr)
 
 ACTIVATION_FUNCTIONS = {
     'relu': torch.nn.ReLU,
@@ -27,7 +107,7 @@ dataset = [
     Dataset('mnist', partial(torchvision.datasets.MNIST, root='./data', train=True, download=True), 1, 28, 10),
     Dataset('cifar10', partial(torchvision.datasets.CIFAR10, root='./data', train=True, download=True), 3, 32, 10),
     Dataset('cifar100', partial(torchvision.datasets.CIFAR100, root='./data', train=True, download=True), 3, 32, 100),
-    Dataset('celeba', partial(torchvision.datasets.CelebA, root='./data', split='train', download=True), 3, 64, 40),
+    Dataset('celeba', partial(CelebAHQ, root='./data/CelebAMask-HQ'), 3, 64, 40),
 ]
 DATASETS = {d.name: d for d in dataset}
 
